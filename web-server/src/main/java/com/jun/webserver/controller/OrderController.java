8471cd56492fa8ed71e221ee47e52ebe28d3b3f5
jun562 <capboy290@naver.com>
2025-11-17 17:42:12 +0900

    feat(orderController): 주문 완료 알림 로직 추가

--- web-server/src/main/java/com/jun/webserver/controller/OrderController.java
+++ web-server/src/main/java/com/jun/webserver/controller/OrderController.java
@@ -5,6 +5,8 @@ import com.jun.grpc.order.CreateOrderResponse;
 import com.jun.grpc.order.OrderServiceGrpc;
 import com.jun.webserver.dto.OrderRequestDto;
 import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
     @GrpcClient("order-service")
     private OrderServiceGrpc.OrderServiceBlockingStub orderStub;
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
 
     @PostMapping("/orders")
     public String createOrder(@RequestBody OrderRequestDto requestDto) {
@@ -25,6 +30,9 @@ public class OrderController {
 
         CreateOrderResponse response = orderStub.createOrder(grpcRequest);
 
+        String notificationMessage = "새 주문! [" + requestDto.getMenuName() + "] 주문번호: " + response.getOrderId();
+        messagingTemplate.convertAndSend("/sub/orders", notificationMessage);
+        
         return "주문 성공! 주문번호: " + response.getOrderId();
     }
 }
