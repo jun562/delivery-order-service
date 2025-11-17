55f0341498840449e5f9a4a191af6f4767829a0a
jun562 <capboy290@naver.com>
2025-11-17 14:39:56 +0900

    feat(order): 주문내역 도메인 추가

---
+++ order-server/src/main/java/com/jun/orderserver/domain/Order.java
@@ -0,0 +1,28 @@
+package com.jun.orderserver.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+    @Id
+    @Column(name = "order_id") // UUID를 문자열로 저장
+    private String orderId;
+
+    private String customerId;
+    private String restaurantId;
+    private String menuName;
+    private Integer price;
+    private String status;
+}
