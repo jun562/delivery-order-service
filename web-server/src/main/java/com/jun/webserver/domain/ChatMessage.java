e85e41667a2e1ceabc4f4339152a054faf532b21
jun562 <capboy290@naver.com>
2025-11-18 12:22:57 +0900

    feat(chatMessage): 채팅 메시지 도메인 생성

---
+++ web-server/src/main/java/com/jun/webserver/domain/ChatMessage.java
@@ -0,0 +1,30 @@
+package com.jun.webserver.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "chat_messages")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String orderId;
+    private String sender;
+    private String content;
+
+    private LocalDateTime timestamp;
+}
